c670d5672036b4cb3f04cacb579ef5a4354c820d
Dan Meyer <daniel.meyer@columbia.edu>
2025-01-31 12:25:35 -0500

    Got rid of runtimewarning

--- scglue/models/glue.py
+++ scglue/models/glue.py
@@ -308,8 +308,6 @@ class GLUETrainer(Trainer):
         self.required_losses += ["dsc_loss", "vae_loss", "gen_loss"]
         self.earlystop_loss = "vae_loss"
 
-        raise RuntimeWarning("Confirming message pass", kwargs)
-
         self.lam_data = lam_data
         self.lam_kl = lam_kl
         self.lam_graph = lam_graph
