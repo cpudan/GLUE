ac4c1675f7371dd87e3c0a6f19b55f0f9a499bf6
Dan Meyer <daniel.meyer@columbia.edu>
2025-01-31 12:14:49 -0500

    Removed message passing errror

--- scglue/models/glue.py
+++ scglue/models/glue.py
@@ -308,7 +308,7 @@ class GLUETrainer(Trainer):
         self.required_losses += ["dsc_loss", "vae_loss", "gen_loss"]
         self.earlystop_loss = "vae_loss"
 
-        raise RuntimeError("Confirming message pass", kwargs)
+        raise RuntimeWarning("Confirming message pass", kwargs)
 
         self.lam_data = lam_data
         self.lam_kl = lam_kl
